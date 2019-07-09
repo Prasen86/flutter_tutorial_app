4527e641dfc0450054cb1a369b60e6c67c752e2d
Rhythm Solutions <rhythmsolutions2017@gmail.com>
2019-07-09 23:52:14 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+Widget _bodyWidget() {
+  return new Container(
+    color: Colors.white,
+    child: Center(
+      child: new Image(
+        image: AssetImage("assets/images/iampoor.png"),
+        fit: BoxFit.fill,
+      ),
+    ),
+  );
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Center(child: new Text("I AM POOR")),
+        backgroundColor: Colors.red,
+      ),
+      body: _bodyWidget(),
+    );
+  }
+}
