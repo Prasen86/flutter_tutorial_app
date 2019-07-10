c32c7aa243bf176d1b1fff4c1a801cebdd31e147
Rhythm Solutions <rhythmsolutions2017@gmail.com>
2019-07-10 21:59:19 +0530

    Adding Photo CircleAvatar

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,20 @@ class HomePage extends StatefulWidget {
 
 Widget _bodyWidget() {
   return Container(
-    color: Colors.teal,
-    child: new Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.all(10.0),
-          width: 100.0,
-          color: Colors.red,
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 100.0,
-              height: 100.0,
-              color: Colors.yellow,
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              color: Colors.green,
-            ),
-          ],
-        ),
-        Container(
-          width: 100.0,
-          color: Colors.blue,
-          margin: EdgeInsets.all(10.0),
-        ),
-      ],
+    color: Colors.black,
+    height: double.infinity,
+    width: double.infinity,
+    child: SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50.0,
+            backgroundColor: Colors.red,
+            backgroundImage: AssetImage('assets/images/rhythm.jpg'),
+          )
+        ],
+      ),
     ),
   );
 }
@@ -57,10 +40,6 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: Center(child: new Text("I AM POOR")),
-        backgroundColor: Colors.red,
-      ),
       body: _bodyWidget(),
     );
   }
