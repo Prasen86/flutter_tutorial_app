5f250baf500d09c1e2e95a4e1c1c125bd78c75b4
Rhythm Solutions <rhythmsolutions2017@gmail.com>
2019-07-10 20:35:27 +0530

    Original Commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,38 @@ class HomePage extends StatefulWidget {
 }
 
 Widget _bodyWidget() {
-  return new Container(
-    color: Colors.white,
-    child: Center(
-      child: new Image(
-        image: AssetImage("assets/images/iampoor.png"),
-        fit: BoxFit.fill,
-      ),
+  return Container(
+    color: Colors.teal,
+    child: new Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          width: 100.0,
+          color: Colors.red,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.green,
+            ),
+          ],
+        ),
+        Container(
+          width: 100.0,
+          color: Colors.blue,
+          margin: EdgeInsets.all(10.0),
+        ),
+      ],
     ),
   );
 }
