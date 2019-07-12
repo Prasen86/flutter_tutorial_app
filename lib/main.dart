e4b251c63b3caede9f56d74cc4282e46691874d9
Rhythm Solutions <rhythmsolutions2017@gmail.com>
2019-07-12 22:28:25 +0530

    First Commit...

--- lib/main.dart
+++ lib/main.dart
@@ -18,83 +18,8 @@ class HomePage extends StatefulWidget {
 
 Widget _bodyWidget() {
   return Container(
-    color: Colors.black,
-    height: double.infinity,
-    width: double.infinity,
-    child: SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          CircleAvatar(
-            radius: 50.0,
-            backgroundColor: Colors.red,
-            backgroundImage: AssetImage('assets/images/rhythm.jpg'),
-          ),
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: new Text(
-              "RHYTHM SHARMA",
-              style: TextStyle(
-                  color: Colors.white, fontSize: 20.0, fontFamily: 'Pacifico'),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: new Text(
-              "Think Big Think Different",
-              style: TextStyle(
-                  letterSpacing: 3.0,
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontFamily: 'SourceSansPro',
-                  fontStyle: FontStyle.italic),
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-            width: 150.0,
-            child: Divider(
-              color: Colors.white,
-            ),
-          ),
-          Container(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-            padding: EdgeInsets.all(10.0),
-            child: Row(
-              children: <Widget>[
-                Icon(
-                  Icons.phone_android,
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Text(
-                  "Phone No.",
-                  style: TextStyle(
-                    fontFamily: "SourceSansPro",
-                    fontSize: 20.0,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.all(10.0),
-            child: ListTile(
-              leading: Icon(
-                Icons.email,
-              ),
-              title: Text(
-                "Email",
-                style: TextStyle(fontFamily: "SourceSansPro", fontSize: 20.0),
-              ),
-            ),
-          ),
-        ],
-      ),
-    ),
+    color: Colors.red,
+    child: Text('HELLO'),
   );
 }
 
@@ -102,6 +27,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "DICEE",
+          style: TextStyle(
+            fontFamily: 'Pacifico',
+            color: Colors.white,
+          ),
+        ),
+      ),
       body: _bodyWidget(),
     );
   }
