c4e7e7fea3f3e6e2c631b68ecba174e9767cf421
Rhythm Solutions <rhythmsolutions2017@gmail.com>
2019-07-15 20:32:46 +0530

    Images Added

--- lib/main.dart
+++ lib/main.dart
@@ -7,112 +7,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-//Widget used earlier wont use setState
-Widget _bodyWidget() {
-  return Center(
-    child: Row(
-      children: <Widget>[
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FlatButton(
-              child: Image.asset('assets/images/dice 1.png'),
-              onPressed: () {
-                print('The left butt');
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: FlatButton(
-            child: Image.asset('assets/images/dice 2.png'),
-            onPressed: () {
-              print('The right butt');
-            },
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-//new Class to use SetState Method
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftdicenumber = 1;
-  int rightdicenumber = 1;
-
-  void changeDiceFace() {
-    setState(() {
-      rightdicenumber = Random().nextInt(6) + 1;
-      leftdicenumber = Random().nextInt(6) + 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(
-                child: Image.asset('assets/images/dice $leftdicenumber.png'),
-                onPressed: () {
-                  changeDiceFace();
-                },
+      home: new Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          title: Center(
+            child: Text(
+              'Ask Me Anything',
+              style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                color: Colors.white,
               ),
             ),
           ),
-          Expanded(
-            child: FlatButton(
-              child: Image.asset('assets/images/dice $rightdicenumber.png'),
-              onPressed: () {
-                setState(
-                  () {
-                    rightdicenumber = Random().nextInt(6) + 1;
-                  },
-                );
-              },
-            ),
-          ),
-        ],
+        ),
+        body: displayAnswer(),
       ),
     );
   }
 }
 
-class _HomePageState extends State<HomePage> {
+class displayAnswer extends StatefulWidget {
+  @override
+  _displayAnswerState createState() => _displayAnswerState();
+}
+
+class _displayAnswerState extends State<displayAnswer> {
+  int ballno = 1;
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text(
-          "DICEE",
-          style: TextStyle(
-            fontFamily: 'Pacifico',
-            color: Colors.white,
-          ),
-        ),
+    return Center(
+      child: RaisedButton(
+        child: Image.asset('assets/images/ball$ballno.png'),
+        onPressed: () {
+          setState(() {
+            ballno = Random().nextInt(5) + 1;
+          });
+        },
       ),
-      body: DicePage(),
     );
   }
 }
