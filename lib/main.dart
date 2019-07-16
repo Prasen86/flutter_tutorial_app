79f6e5b53a8f4a14e5fe3f1c8c572bd23666b810
Rhythm Solutions <rhythmsolutions2017@gmail.com>
2019-07-17 00:03:37 +0530

    Classes Built

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'StoryBrain.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,23 +12,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
+BoxDecoration _background() {
+  return BoxDecoration(
+    image: DecorationImage(
+      image: ExactAssetImage('assets/images/background.png'),
+      fit: BoxFit.cover,
+    ),
+  );
+}
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  StoryBrain storyBrain = StoryBrain();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: ExactAssetImage('assets/images/background.png'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-
+    return Scaffold(
+      body: Container(
+        decoration: _background(),
+        child: SafeArea(
+            minimum: EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  flex: 12,
+                  child: Center(
+                    child: Text(
+                      storyBrain.getStory(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      side: BorderSide(style: BorderStyle.solid, width: 2.0),
+                    ),
+                    onPressed: () {},
+                    color: Colors.red,
+                    child: Text('CHOICE 1'),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Expanded(
+                  flex: 3,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      side: BorderSide(style: BorderStyle.solid, width: 2.0),
+                    ),
+                    onPressed: () {},
+                    color: Colors.green,
+                    child: Text('CHOICE 2'),
+                  ),
+                ),
+              ],
+            )),
       ),
     );
   }
